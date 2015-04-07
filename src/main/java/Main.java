bbf2c4ad9ef10b2a865f6e99183e61d44a4d3b49
mariusz_kubis <mariusz,kubis@cognifide.com>
2015-04-07 16:07:18 +0200

    coverity_scan

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,5 +7,9 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("Hello");
+        if(false)
+        {
+            System.out.print("false");
+        }
     }
 }
