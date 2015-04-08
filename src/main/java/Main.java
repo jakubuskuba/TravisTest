cd4854072a06c371f025f9dc496154ebdc7d8616
mariusz_kubis <mariusz,kubis@cognifide.com>
2015-04-08 11:59:16 +0200

    slice-2 brunch pattern test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,6 +9,7 @@ public class Main {
         System.out.println("Hello");
         System.out.println("Hello1");
         System.out.println("Hello2");
+        System.out.println("Hello3");
 
     }
 }
