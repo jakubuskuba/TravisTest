5ac64f8d295103c8c124fcc214c70a5a8dfe21e5
mariusz_kubis <mariusz.kubis.aha@gmail.com>
2015-04-28 10:51:16 +0200

    jacoco multi module

---
+++ first-module/src/test/java/FirstTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+/**
+ * @author Mariusz Kubiś Date: 28.04.15
+ */
+public class FirstTest {
+
+	@Test
+	public void multiTest() {
+		First first = new First();
+		Second second = new Second();
+	}
+
+}
---
+++ second-module/src/main/java/Second.java
@@ -0,0 +1,9 @@
+/**
+ * @author Mariusz Kubiś Date: 28.04.15
+ */
+public class Second {
+
+	public Second() {
+		System.out.println("second");
+	}
+}
