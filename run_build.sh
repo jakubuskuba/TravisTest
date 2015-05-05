7270ad9409c46008bbbe52cedb53a11300732ddc
mariusz_kubis <mariusz.kubis.aha@gmail.com>
2015-05-05 12:52:05 +0200

    test1

--- run_build.sh
+++ run_build.sh
@@ -2,8 +2,12 @@
 
 set -ev
 
+echo $COVERITY_SCAN_PROJECT_NAME
+echo $SCAN_URL
 AUTH_RES=`curl -s --form project="jakubuskuba/TravisTest" --form token="$COVERITY_SCAN_TOKEN" https://scan.coverity.com/api/upload_permitted`
 AUTH=`echo $AUTH_RES | ruby -e "require 'rubygems'; require 'json'; puts JSON[STDIN.read]['upload_permitted']"`
+echo $AUTH_RES
+echo $AUTH
 echo "master1"
 
 
